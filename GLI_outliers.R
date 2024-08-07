e518f6adb720911ea42fbf6464a16538bc64b017
Nick Shrine <nick.shrine@leicester.ac.uk>
2024-08-07 07:11:08 +0100

    GLI outliers function

--- GLI_outliers.R
+++ GLI_outliers.R
@@ -1,13 +1,5 @@
-library(tidyverse)
-library(rspiro) 
-
-args <- commandArgs(TRUE)
-argc <- length(args)
-
-LF_file <- args[1]
-ethnicity <- ifelse(argc > 1, as.integer(args[2]), 1)
-
-LF <- read_tsv(LF_file)
+require(tidyverse)
+require(rspiro) 
 
 # Arguments to rspiro function zscore_GLI
 
@@ -19,15 +11,16 @@ LF <- read_tsv(LF_file)
 # FVC	        Forced Vital Capacity (lt)
 # FEV1FVC	    FEV1 / FVC 
 
-
 # Set up the required variables in the correct units for Z score calculation
-LF_outliers <- LF %>%
-  mutate(age = age_at_recruitment,
-         gender = 2 - sex,
-         height = standing_height / 100,
-         fev1_zscore = zscore_GLI(age, height, gender, ethnicity, FEV1=fev1_best),
-         fvc_zscore = zscore_GLI(age, height, gender, ethnicity, FVC=fvc_best),
-         ratio_zscore = zscore_GLI(age, height, gender, ethnicity, FEV1FVC=ratio_best),
-         max_zscore = pmax(abs(fev1_zscore), abs(fvc_zscore), abs(ratio_zscore)),
-         outlier = max_zscore > 5)
-  
+GLI_outliers <- function(LF, ethnicity=1, SDs=5) {
+    LF %>%
+        mutate(age = age_at_recruitment,
+               gender = 2 - sex,
+               height = standing_height / 100,
+               fev1_zscore = zscore_GLI(age, height, gender, ethnicity, FEV1=fev1_best),
+               fvc_zscore = zscore_GLI(age, height, gender, ethnicity, FVC=fvc_best),
+               ratio_zscore = zscore_GLI(age, height, gender, ethnicity, FEV1FVC=ratio_best),
+               max_zscore = pmax(abs(fev1_zscore), abs(fvc_zscore), abs(ratio_zscore)),
+               outlier = max_zscore > 5) %>%
+    select(-age, -gender, -height, -ends_with("_zscore"))
+}
